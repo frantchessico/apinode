2f6496f789f82d89a4c391f54065fd44e634baaa
Francisco Inoque <tchessico@live.com>
2020-09-07 23:24:57 -0300

    config datatbase

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const urlMdb = 'mongodb://localhost/todo';
+const prodUrl = 'mongodb+srv://nodeapi:nodeapi@cluster0.vx5qo.mongodb.net/nodeapi?retryWrites=true&w=majority'
 
-mongoose.connect(urlMdb,{
+mongoose.connect(prodUrl,{
    useNewUrlParser: true,
    useUnifiedTopology: true
 }).then(() => console.log('Database is connected'))
